c142d8054791bd6596ddf7ad744b775465333e8d
熊谷洸希 <>
2018-12-23 15:54:01 +0900

    ユーザ認証追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/me'
+  get 'home/index'
+  get 'sessions/create'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'home#index'
   resources :comments, only: %i[create destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
